b155dd09acd180a06e9ab57ebed144cd7a257ca8
WaltBox <waltboxwell@gmail.com>
2023-06-12 18:45:20 -0500

    IDK WHAT I AM DOING

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   resources :profiles
-  devise_for :users 
+  devise_for :users
 
   root "home_page#index"
 
   patch 'profile/:id/edit', to: 'profiles#update', as: 'update_user_profile'
   get 'profile/:id/edit', to: 'profiles#edit', as: 'edit_user_profile'
   get 'profile/:id', to: 'profiles#show', as: 'user_profile'
-  
-  get 'houses/:id', to: 'houses#show', as: 'user_house'
 
+  resources :users do
+    resources :houses, only: [:show]
+  end
+  
+  resources :houses, only: [:show, :new]
+  get 'house/join', to: 'houses#join', as: 'join_house'
+  post 'house/create', to: 'houses#create', as: 'create_house'
 end
