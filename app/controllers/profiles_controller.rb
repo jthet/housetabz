91938e76482f0a7ef4803d482da43cd447d426e1
WaltBox <waltboxwell@gmail.com>
2023-07-13 19:42:11 -0500

    minor updates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,54 +1,48 @@
 class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    puts params.inspect
+    @profile = current_user.build_profile(profile_params)
     
-    def new
-        @profile = Profile.new
+    if @profile.save
+      redirect_to user_profile_path(current_user), notice: 'Profile created successfully.'
+    else
+      render :new
     end
-    
-    def create
-      @profile = current_user.build_profile(profile_params)
-      if @profile.save
-        redirect_to user_profile_path(current_user), notice: 'Profile created successfully.'
-      else
-        render :new
-      end
-    end
-  
-      
-    
-def show
+  end
+
+  def show
     @user = current_user
     @profile = @user.profile
-  
+
     # You can add additional logic if the profile is nil or not found
     if @profile.nil?
       # Handle the case when the profile is not found
     end
   end
-  
 
-    def edit 
-      @user = current_user
-      @profile = @user.profile
+  def edit
+    @user = current_user
+    @profile = @user.profile
+  end
+
+  def update
+    @user = current_user
+    @profile = @user.profile
+
+    if @profile.update(profile_params)
+      redirect_to user_profile_path(current_user), notice: 'Profile was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-  
-    def update
-      @user = current_user
-      @profile = @user.profile
-    
-      if @profile.update(profile_params)
-        redirect_to user_profile_path(current_user), notice: 'Profile was successfully updated.'
-      else
-        render :edit
-      end
-    end
-    
+  private
 
-  
-
-private
-
-def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :bio, :profile_picture)
-end
+  def profile_params
+    params.permit(:first_name, :last_name, :bio, :profile_picture).merge(user_id: current_user.id)
+  end
 end
