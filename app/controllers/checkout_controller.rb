0f1851ae94349452e19e4d65fab39fd6544c357b
WaltBox <waltboxwell@gmail.com>
2023-08-23 13:59:22 -0500

    need to merge to main

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -11,8 +11,8 @@ class CheckoutController < ApplicationController
       return
     end
 
-    cancel_url = 'https://688b-107-178-28-71.ngrok-free.app/checkout'  # Replace with your cancel URL
-    success_url = "https://688b-107-178-28-71.ngrok-free.app/checkout_success?user_id=#{user_id}&price=#{custom_price}"
+    cancel_url = 'https://e285-24-243-149-225.ngrok-free.app/checkout'  # Replace with your cancel URL
+    success_url = "https://e285-24-243-149-225.ngrok-free.app/checkout_success?user_id=#{user_id}&price=#{custom_price}"
     
     session = Stripe::Checkout::Session.create(
       success_url: success_url,
