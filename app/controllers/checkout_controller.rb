12c64efda861db169187e393b3ce5e539c838c86
WaltBox <waltboxwell@gmail.com>
2023-09-19 15:49:38 -0500

    added heroku

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -11,8 +11,8 @@ class CheckoutController < ApplicationController
       return
     end
 
-    cancel_url = 'https://50c8-2600-387-f-7113-00-9.ngrok-free.app/checkout'  # Replace with your cancel URL
-    success_url = "https://50c8-2600-387-f-7113-00-9.ngrok-free.app/checkout_success?user_id=#{user_id}&price=#{custom_price}"
+    cancel_url = 'https://salty-sierra-05335-bfc9793be84d.herokuapp.com/checkout'  # Replace with your cancel URL
+    success_url = "https://salty-sierra-05335-bfc9793be84d.herokuapp.com/checkout_success?user_id=#{user_id}&price=#{custom_price}"
     
     session = Stripe::Checkout::Session.create(
       success_url: success_url,
