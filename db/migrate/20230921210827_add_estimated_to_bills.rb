d29a723319bdcb5b7e22ba7216d8f7a7e3a315cf
WaltBox <waltboxwell@gmail.com>
2023-09-21 18:15:55 -0500

    adding postgres take 2

--- db/migrate/20230921210827_add_estimated_to_bills.rb
+++ db/migrate/20230921210827_add_estimated_to_bills.rb
@@ -1,5 +1,5 @@
 class AddEstimatedToBills < ActiveRecord::Migration[7.0]
   def change
-    add_column :bills, :estimated, :boolean
+    add_column :bills, :estimated, :boolean, default: false
   end
 end
