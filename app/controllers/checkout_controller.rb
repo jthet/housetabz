3b28e7caad5a3a33157b57c006f7fa970855b542
WaltBox <waltboxwell@gmail.com>
2023-09-27 16:48:31 -0500

    reverted back

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -14,47 +14,47 @@ class CheckoutController < ApplicationController
     cancel_url = 'https://salty-sierra-05335-bfc9793be84d.herokuapp.com//checkout'  # Replace with your cancel URL
     success_url = "https://salty-sierra-05335-bfc9793be84d.herokuapp.com/checkout_success?user_id=#{user_id}&price=#{custom_price}"
     
-#     session = Stripe::Checkout::Session.create(
-#       success_url: success_url,
-#       cancel_url: cancel_url,
-#       payment_method_types: ['card'],
-#       metadata: {
-#         user_id: user_id.to_s
-#       },
-#       line_items: [{
-#         price_data: {
-#           currency: 'usd',  # Replace 'usd' with the appropriate currency code
-#           product_data: {
-#             name: 'HouseTabz services'  # Replace with the actual product name
-#           },
-#           unit_amount: (custom_price * 100).to_i  # Convert the price to cents
-#         },
-#         quantity: 1
-#       }],
-#       mode: 'payment'
-#     )
+    session = Stripe::Checkout::Session.create(
+      success_url: success_url,
+      cancel_url: cancel_url,
+      payment_method_types: ['card'],
+      metadata: {
+        user_id: user_id.to_s
+      },
+      line_items: [{
+        price_data: {
+          currency: 'usd',  # Replace 'usd' with the appropriate currency code
+          product_data: {
+            name: 'HouseTabz services'  # Replace with the actual product name
+          },
+          unit_amount: (custom_price * 100).to_i  # Convert the price to cents
+        },
+        quantity: 1
+      }],
+      mode: 'payment'
+    )
 
-#     render json: { id: session.id }
-#   rescue Stripe::StripeError => e
-#     render json: { error: e.message }, status: :unprocessable_entity
-#   end
+    render json: { id: session.id }
+  rescue Stripe::StripeError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 
-#   def checkout_success
-#     user_id = params[:user_id].to_i
-#     custom_price = params[:price].to_f
+  def checkout_success
+    user_id = params[:user_id].to_i
+    custom_price = params[:price].to_f
     
-#     webhooks_controller = WebhooksController.new
-#     webhooks_controller.handle_successful_payment(user_id, custom_price)
+    webhooks_controller = WebhooksController.new
+    webhooks_controller.handle_successful_payment(user_id, custom_price)
 
-#     render 'checkout/checkout_success'
-#   end
-#   def checkout_success
-#     user_id = params[:user_id].to_i
-#     custom_price = params[:price].to_f
+    render 'checkout/checkout_success'
+  end
+  def checkout_success
+    user_id = params[:user_id].to_i
+    custom_price = params[:price].to_f
     
-#     webhooks_controller = WebhooksController.new
-#     webhooks_controller.handle_successful_payment(user_id, custom_price)
+    webhooks_controller = WebhooksController.new
+    webhooks_controller.handle_successful_payment(user_id, custom_price)
 
-#     render 'checkout/checkout_success'
-#   end
-# end
+    render 'checkout/checkout_success'
+  end
+end
