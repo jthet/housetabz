0d0978c5b49af5f5c77408de1bf304a2009cf1bc
Jackson Thetford <j.thetford@utexas.edu>
2023-10-12 18:48:42 -0500

    Fixed ammount data type

--- db/migrate/20231011030839_change_amount_column_type_in_bills.rb
+++
@@ -1,9 +0,0 @@
-class ChangeAmountColumnTypeInBills < ActiveRecord::Migration[6.1] # or your corresponding Rails version
-  def up
-    change_column :bills, :amount, 'decimal USING CAST(amount AS decimal)', precision: 8, scale: 2
-  end
-  
-  def down
-    change_column :bills, :amount, :string
-  end
-end
