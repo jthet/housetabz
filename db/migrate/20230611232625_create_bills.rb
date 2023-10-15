a09c012bd1ef313f2a09d0134058b2424b8c993f
WaltBox <waltboxwell@gmail.com>
2023-10-14 22:30:41 -0500

    changed bill migration file to amount integer

--- db/migrate/20230611232625_create_bills.rb
+++ db/migrate/20230611232625_create_bills.rb
@@ -2,8 +2,8 @@ class CreateBills < ActiveRecord::Migration[7.0]
   def change
     create_table :bills do |t|
       t.string :name
-      t.string :amount
-      t.string :decimal
+      t.integer :amount
+      t.decimal :decimal
       t.references :house, null: false, foreign_key: true
 
       t.timestamps
