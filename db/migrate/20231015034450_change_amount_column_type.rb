7fe684ac1317ca845057c3745339e39bd3c4f0a0
WaltBox <waltboxwell@gmail.com>
2023-10-14 22:45:33 -0500

    fix column type?

---
+++ db/migrate/20231015034450_change_amount_column_type.rb
@@ -0,0 +1,5 @@
+class ChangeAmountColumnType < ActiveRecord::Migration[7.0]
+  def change
+    change_column :bills, :amount, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_212043) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_034450) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -84,7 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_212043) do
 
   create_table "bills", force: :cascade do |t|
     t.string "name"
-    t.string "amount"
+    t.integer "amount"
     t.string "decimal"
     t.integer "house_id", null: false
     t.datetime "created_at", null: false
