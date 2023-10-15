bb89111d346e1f74995c6ef62a35b85d614e270a
WaltBox <waltboxwell@gmail.com>
2023-10-14 22:14:57 -0500

    commented out stripe lines in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,12 @@
 # app/controllers/registrations_controller.rb
 class RegistrationsController < Devise::RegistrationsController
-  # def create
-  #   super do |resource|
-  #     # Custom code to create a customer in Stripe and associate the user ID
-  #     customer = Stripe::Customer.create(email: resource.email, metadata: {user_id: resource.id})
-  #   end
-  # end
+  def create
+     super 
+    #do |resource|
+    #   # Custom code to create a customer in Stripe and associate the user ID
+    #   customer = Stripe::Customer.create(email: resource.email, metadata: {user_id: resource.id})
+    #end
+  end
 
   protected
 
