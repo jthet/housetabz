31555d9376b5c13f3e0052f865ec91a80bac588b
WaltBox <waltboxwell@gmail.com>
2023-10-17 20:02:28 -0500

    added house to main

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,8 +1,17 @@
 class House < ApplicationRecord
+
     has_secure_password
     has_many :users
     has_many :bills
     has_many :paid_bills
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["address", "created_at", "id", "joinable", "name", "password_digest", "updated_at", "bills_id"]
+      end
+
+      def self.ransackable_associations(auth_object = nil)
+        ["bills", "paid_bills", "users"]
+      end
 
     def total_charge
         bills.sum(:amount)
--- app/models/paid_bill.rb
+++ app/models/paid_bill.rb
@@ -1,6 +1,10 @@
 class PaidBill < ApplicationRecord
     belongs_to :bill
-  
+    
+    def self.ransackable_attributes(auth_object = nil)
+      ["bill_id", "created_at", "house_id", "id", "updated_at", "user_id"]
+    end
+    
     def self.create_from_bill(bill)
       PaidBill.create(bill: bill, house_id: bill.house_id)
     end
